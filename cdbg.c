45194ec5bae4de08ce61e941ff67798ee338d384
Dymitr <43708417+ay0ks@users.noreply.github.com>
2025-04-19 13:52:21 +0400

    feat(cdbg/cdbg.c, cdbg/cdbg.h): rename from CDebugging/CDebugging.c, CDebugging/CDebugging.h

---
+++ cdbg.c
@@ -0,0 +1,108 @@
+#include "cdbg.h"
+
+#include <assert.h>
+#include <stdarg.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <signal.h>
+
+#if defined(_WIN32) || defined(_WIN64)
+#  define WIN32_LEAN_AND_MEAN
+#  include <windows.h>
+#  include <debugapi.h>
+#endif
+
+void
+cdbg_assert(
+  const char *const a_file,
+  const char *const a_function,
+  const uint64_t a_line,
+  const char *const a_expression,
+  ...
+)
+{
+  va_list l_args;
+  va_start(l_args, a_expression);
+  const char *const l_message = va_arg(l_args, char *);
+  fprintf_s(stderr, "%s:%llu Assertion failed in %s", a_file, a_line, a_function, a_expression);
+  if(l_message != NULL) { fprintf_s(stderr, " (%s)", l_message); }
+  fprintf_s(stderr, "\n");
+  va_end(l_args);
+  cdbg_abort();
+}
+
+void
+cdbg_abort()
+{
+#if defined(_WIN32) || defined(_WIN64)
+  DebugBreak();
+#elif defined(__linux__)
+  raise(SIGTRAP);
+#endif
+  raise(SIGABRT);
+}
+
+void
+cdbg_breakpoint_set(
+  cdbg_breakpoint_t *a_breakpoint,
+  const char *const a_file,
+  const char *const a_function,
+  const uint64_t a_line
+)
+{
+  assert(a_breakpoint != NULL);
+  if(setjmp(a_breakpoint->m_jump_site.m_buffer) == 0)
+  {
+    a_breakpoint->m_armed = true;
+    a_breakpoint->m_set_site.m_file = a_file;
+    a_breakpoint->m_set_site.m_function = a_function;
+    a_breakpoint->m_set_site.m_line = a_line;
+  }
+  else
+  {
+    fprintf_s(
+      stderr,
+      "Breakpoint set in %s:%s at %llu triggered in %s:%s at %llu\n",
+      a_breakpoint->m_set_site.m_file,
+      a_breakpoint->m_set_site.m_function,
+      a_breakpoint->m_set_site.m_line,
+      a_breakpoint->m_jump_site.m_file,
+      a_breakpoint->m_jump_site.m_function,
+      a_breakpoint->m_jump_site.m_line
+    );
+    cdbg_abort();
+  }
+}
+
+void
+cdg_breakpoint_break(
+  cdbg_breakpoint_t *a_breakpoint,
+  const char *const a_file,
+  const char *const a_function,
+  const uint64_t a_line
+)
+{
+  assert(a_breakpoint != NULL);
+  if(a_breakpoint->m_armed)
+  {
+    a_breakpoint->m_armed = false;
+    a_breakpoint->m_jump_site.m_file = a_file;
+    a_breakpoint->m_jump_site.m_function = a_function;
+    a_breakpoint->m_jump_site.m_line = a_line;
+    longjmp(a_breakpoint->m_jump_site.m_buffer, 1);
+  }
+}
+
+void
+cdbg_breakpoint_clear(
+  cdbg_breakpoint_t *a_breakpoint
+)
+{
+  assert(a_breakpoint != NULL);
+  a_breakpoint->m_armed = false;
+  memset(
+    &a_breakpoint->m_jump_site.m_buffer, 0, sizeof(a_breakpoint->m_jump_site.m_buffer)
+  );
+  memset(&a_breakpoint->m_set_site, 0, sizeof(a_breakpoint->m_set_site));
+  memset(&a_breakpoint->m_jump_site, 0, sizeof(a_breakpoint->m_jump_site));
+}
---
+++ cdbg.h
@@ -0,0 +1,70 @@
+#pragma once
+
+#include <setjmp.h>
+#include <stdbool.h>
+#include <stdint.h>
+
+#define assert($Expression, ...)                                               \
+  ((void)((!!($Expression))                                                    \
+          || (cdbg_assert(__FILE__, __func__, __LINE__, #$Expression, __VA_ARGS__))))
+
+#define assert_equals($Left, $Right, ...)                                      \
+  ((void)((!!($Left == $Right))                                                \
+          || (cdbg_assert(__FILE__, __func__, __LINE__, #$Left " == " #$Right, __VA_ARGS__))))
+
+void
+cdbg_assert(
+  const char *a_file,
+  const char *a_function,
+  uint64_t a_line,
+  const char *a_expression,
+  ...
+);
+
+void
+cdbg_abort();
+
+typedef struct cdbg_breakpoint_s
+{
+  bool m_armed;
+  struct
+  {
+    const char *m_file;
+    const char *m_function;
+    uint64_t m_line;
+  } m_set_site;
+  struct
+  {
+    jmp_buf m_buffer;
+    const char *m_file;
+    const char *m_function;
+    uint64_t m_line;
+  } m_jump_site;
+} cdbg_breakpoint_t;
+
+void
+cdbg_breakpoint_set(
+  cdbg_breakpoint_t *a_breakpoint,
+  const char *a_file,
+  const char *a_function,
+  uint64_t a_line
+);
+
+void
+cdbg_breakpoint_break(
+  cdbg_breakpoint_t *a_breakpoint,
+  const char *a_file,
+  const char *a_function,
+  uint64_t a_line
+);
+
+void
+cdbg_breakpoint_clear(cdbg_breakpoint_t *a_breakpoint);
+
+#define breakpoint_set($Breakpoint)                                            \
+  (cdbg_breakpoint_set($Breakpoint, __FILE__, __func__, __LINE__))
+
+#define breakpoint_trigger($Breakpoint)                                        \
+  (cdbg_breakpoint_break($Breakpoint, __FILE__, __func__, __LINE__))
+
+#define breakpoint_clear($Breakpoint) (cdbg_breakpoint_clear($Breakpoint))
