25d1c72654861b788b507ddc65389ed82506af25
Dymitr <43708417+ay0ks@users.noreply.github.com>
2025-04-26 20:27:49 +0400

    fix(cdbg/cdbg.c): char to wchar conversion

--- cdbg.c
+++ cdbg.c
@@ -6,6 +6,7 @@
 #include <stdio.h>
 #include <signal.h>
 #include <string.h>
+#include <locale.h>
 
 #if defined(_WIN32) || defined(_WIN64)
 #  define WIN32_LEAN_AND_MEAN
@@ -19,6 +20,8 @@
 #  define cdbg_fprintf fwprintf
 #endif
 
+bool g_locale_set = false;
+
 void
 cdbg_assert(
   const char *const a_file,
@@ -29,11 +32,24 @@ cdbg_assert(
   ...
 )
 {
+  if(!g_locale_set) { setlocale(LC_ALL, ""); }
   va_list l_args;
   va_start(l_args, a_abort);
+  uint64_t l_file_length = strlen(a_file);
+  wchar_t l_file[l_file_length + 1];
+  mbstowcs(l_file, a_file, l_file_length);
+  l_file[l_file_length] = L'\0';
+  uint64_t l_function_length = strlen(a_function);
+  wchar_t l_function[l_function_length + 1];
+  mbstowcs(l_function, a_function, l_function_length);
+  l_function[l_function_length] = L'\0';
+  uint64_t l_expression_length = strlen(a_expression);
+  wchar_t l_expression[l_expression_length + 1];
+  mbstowcs(l_expression, a_expression, l_expression_length);
+  l_expression[l_expression_length] = L'\0';
   const wchar_t *const l_message = va_arg(l_args, wchar_t *);
   cdbg_fprintf(
-    stderr, L"%s:%llu Assertion failed in %s", a_file, a_line, a_function, a_expression
+    stderr, L"%s:%llu Assertion failed in %s", l_file, a_line, l_function, l_expression
   );
   if(l_message != NULL) { cdbg_fprintf(stderr, L" (%s)", l_message); }
   cdbg_fprintf(stderr, L"\n");
@@ -64,8 +80,16 @@ cdbg_breakpoint_set(
   if(setjmp(a_breakpoint->m_jump_site.m_buffer) == 0)
   {
     a_breakpoint->m_armed = true;
-    a_breakpoint->m_set_site.m_file = a_file;
-    a_breakpoint->m_set_site.m_function = a_function;
+    uint64_t l_file_length = strlen(a_file);
+    wchar_t l_file[l_file_length + 1];
+    mbstowcs(l_file, a_file, l_file_length);
+    l_file[l_file_length] = L'\0';
+    uint64_t l_function_length = strlen(a_function);
+    wchar_t l_function[l_function_length + 1];
+    mbstowcs(l_function, a_function, l_function_length);
+    l_function[l_function_length] = L'\0';
+    a_breakpoint->m_set_site.m_file = l_file;
+    a_breakpoint->m_set_site.m_function = l_function;
     a_breakpoint->m_set_site.m_line = a_line;
   }
   else
@@ -96,8 +120,16 @@ cdg_breakpoint_break(
   if(a_breakpoint->m_armed)
   {
     a_breakpoint->m_armed = false;
-    a_breakpoint->m_jump_site.m_file = a_file;
-    a_breakpoint->m_jump_site.m_function = a_function;
+    uint64_t l_file_length = strlen(a_file);
+    wchar_t l_file[l_file_length + 1];
+    mbstowcs(l_file, a_file, l_file_length);
+    l_file[l_file_length] = L'\0';
+    uint64_t l_function_length = strlen(a_function);
+    wchar_t l_function[l_function_length + 1];
+    mbstowcs(l_function, a_function, l_function_length);
+    l_function[l_function_length] = L'\0';
+    a_breakpoint->m_set_site.m_file = l_file;
+    a_breakpoint->m_set_site.m_function = l_function;
     a_breakpoint->m_jump_site.m_line = a_line;
     longjmp(a_breakpoint->m_jump_site.m_buffer, 1);
   }
