db5c24c1fed06defb5d3608f3559632e224c0c28
Dymitr <43708417+ay0ks@users.noreply.github.com>
2025-04-28 15:10:34 +0400

    fix(cdbg/cdbg.h): macros

--- cdbg.h
+++ cdbg.h
@@ -5,22 +5,28 @@
 #include <stdbool.h>
 #include <stdint.h>
 
-#define assert($Expression, ...)                                                               \
-  ((void)((!!($Expression))                                                                    \
-          || (cdbg_assert(__FILE__, __func__, __LINE__, #$Expression, true, ##__VA_ARGS__), 0) \
+#define __cdbg_stringify($Value) #$Value
+#define __cdbg_stringify_widen($Value) L##$Value
+
+#define cdbg_stringify($Value) __cdbg_stringify($Value)
+#define cdbg_stringify_widen($Value) __cdbg_stringify_widen($Value)
+
+#define assert($Expression, ...)                                                                                                                                    \
+  ((void)((!!($Expression))                                                                                                                                         \
+          || (cdbg_assert((cdbg_stringify_widen(__FILE__)), (__func__), (__LINE__), (cdbg_stringify_widen(cdbg_stringify($Expression))), true, ##__VA_ARGS__), (0)) \
   ))
 
-#define assert_soft($Expression, ...)                                                           \
-  ((void)((!!($Expression))                                                                     \
-          || (cdbg_assert(__FILE__, __func__, __LINE__, #$Expression, false, ##__VA_ARGS__), 0) \
+#define assert_soft($Expression, ...)                                                                                                                                \
+  ((void)((!!($Expression))                                                                                                                                          \
+          || (cdbg_assert((cdbg_stringify_widen(__FILE__)), (__func__), (__LINE__), (cdbg_stringify_widen(cdbg_stringify($Expression))), false, ##__VA_ARGS__), (0)) \
   ))
 
 void
 cdbg_assert(
-  const char *a_file,
+  const wchar_t *a_file,
   const char *a_function,
   uint64_t a_line,
-  const char *a_expression,
+  const wchar_t *a_expression,
   bool a_abort,
   ...
 );
@@ -30,35 +36,35 @@ cdbg_abort();
 
 typedef struct cdbg_dump_lookaround_s
 {
-  uint8_t m_dummy;
+  uint8_t m_dummy : 1;
   uint64_t m_lookbehind;
   uint64_t m_lookahead;
 } cdbg_dump_lookaround_t;
 
 #define dump($Value, $ValueSize, ...)                                                    \
   ((void)({                                                                              \
-    char l_address[48];                                                                  \
-    snprintf(l_address, sizeof(l_address), "%p", $Value);                                \
+    wchar_t l_address[48];                                                               \
+    ((void)(snwprintf(l_address, sizeof(l_address), L"%p", $Value)));                    \
     ((void)(cdbg_dump(                                                                   \
-      __FILE__,                                                                          \
-      __func__,                                                                          \
-      __LINE__,                                                                          \
-      l_address,                                                                         \
+      (cdbg_stringify_widen(__FILE__)),                                                  \
+      (__func__),                                                                        \
+      (__LINE__),                                                                        \
+      (l_address),                                                                       \
       (cdbg_dump_lookaround_t){0, ##__VA_ARGS__},                                        \
-      #$Value,                                                                           \
+      (cdbg_stringify_widen(cdbg_stringify($Value))),                                    \
       ($ValueSize),                                                                      \
-      (char *)($Value)                                                                   \
+      ((char *)($Value))                                                                 \
     )));                                                                                 \
   }))
 
 void
 cdbg_dump(
-  const char *a_file,
+  const wchar_t *a_file,
   const char *a_function,
   uint64_t a_line,
-  const char *a_address,
+  const wchar_t *a_address,
   cdbg_dump_lookaround_t a_lookaround,
-  const char *a_value_repr,
+  const wchar_t *a_value_repr,
   uint64_t a_size,
   const char *a_value
 );
@@ -84,7 +90,7 @@ typedef struct cdbg_breakpoint_s
 void
 cdbg_breakpoint_set(
   cdbg_breakpoint_t *a_breakpoint,
-  const char *a_file,
+  const wchar_t *a_file,
   const char *a_function,
   uint64_t a_line
 );
@@ -92,7 +98,7 @@ cdbg_breakpoint_set(
 void
 cdbg_breakpoint_break(
   cdbg_breakpoint_t *a_breakpoint,
-  const char *a_file,
+  const wchar_t *a_file,
   const char *a_function,
   uint64_t a_line
 );
@@ -101,9 +107,13 @@ void
 cdbg_breakpoint_clear(cdbg_breakpoint_t *a_breakpoint);
 
 #define breakpoint_set($Breakpoint)                                                      \
-  (cdbg_breakpoint_set($Breakpoint, __FILE__, __func__, __LINE__))
+  (cdbg_breakpoint_set(                                                                  \
+    ($Breakpoint), (cdbg_stringify_wide(__FILE__)), (__func__), (__LINE__)               \
+  ))
 
 #define breakpoint_trigger($Breakpoint)                                                  \
-  (cdbg_breakpoint_break($Breakpoint, __FILE__, __func__, __LINE__))
+  (cdbg_breakpoint_break(                                                                \
+    ($Breakpoint), (cdbg_stringify_wide(__FILE__)), (__func__), (__LINE__)               \
+  ))
 
-#define breakpoint_clear($Breakpoint) (cdbg_breakpoint_clear($Breakpoint))
+#define breakpoint_clear($Breakpoint) (cdbg_breakpoint_clear(($Breakpoint)))
