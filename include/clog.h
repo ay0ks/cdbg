8129c73fdfb7bc3dd67813067960f5d3b5dd5c86
Dymitr <43708417+ay0ks@users.noreply.github.com>
2025-05-28 02:17:00 +0200

    fix(clog/clog.h): no need for a_argc

--- include/clog.h
+++ include/clog.h
@@ -6,52 +6,34 @@
 #include <stdbool.h>
 #include <stdint.h>
 
-#define __clog_count_helper_2($1, ...) ((typeof($1)[]){$1, __VA_ARGS__})
-#define __clog_count_helper_1(...)                                             \
-  ((uint64_t)(sizeof(__VA_ARGS__) / sizeof(*(__VA_ARGS__))))
-#define __clog_count(...)                                                      \
-  (0 __VA_OPT__(+__clog_count_helper_1(__clog_count_helper_2(__VA_ARGS__))))
-
 #define __clog_stringify_helper($Value) #$Value
 #define __clog_stringify($Value) __clog_stringify_helper($Value)
 
-#define __clog_assert($Expression, $Abort, $ArgCount, ...)                                                          \
-  ((void)({                                                                                                         \
-    do {                                                                                                            \
-      if(!($Expression))                                                                                            \
-      {                                                                                                             \
-        clog_assert(                                                                                                \
-          (__FILE__), (__func__), (__LINE__), (__clog_stringify($Expression)), ($Abort), ($ArgCount), ##__VA_ARGS__ \
-        );                                                                                                          \
-      }                                                                                                             \
-    }                                                                                                               \
-    while(0);                                                                                                       \
+#define __clog_assert($Expression, $Abort, ...)                                                                        \
+  ((void)({                                                                                                            \
+    do {                                                                                                               \
+      if(!($Expression))                                                                                               \
+      {                                                                                                                \
+        clog_assert((__FILE__), (__func__), (__LINE__), (__clog_stringify($Expression)), ($Abort), ##__VA_ARGS__);     \
+      }                                                                                                                \
+    }                                                                                                                  \
+    while(0);                                                                                                          \
   }))
 
-#define assert($Expression, ...)                                               \
-  __clog_assert(($Expression), (true), (__clog_count(__VA_ARGS__)), ##__VA_ARGS__)
+#define assert($Expression, ...) __clog_assert(($Expression), (true), ##__VA_ARGS__)
 
-#define assert_soft($Expression, ...)                                          \
-  __clog_assert(($Expression), (false), (__clog_count(__VA_ARGS__)), ##__VA_ARGS__)
+#define assert_soft($Expression, ...) __clog_assert(($Expression), (false), ##__VA_ARGS__)
 
-#define goto_assert($Label, $Expression, ...)                                  \
-  ((void)({                                                                    \
-    do {                                                                       \
-      if(!($Expression))                                                       \
-      {                                                                        \
-        clog_assert(                                                           \
-          (__FILE__),                                                          \
-          (__func__),                                                          \
-          (__LINE__),                                                          \
-          (__clog_stringify($Expression)),                                     \
-          (false),                                                             \
-          (__clog_count(__VA_ARGS__)),                                         \
-          ##__VA_ARGS__                                                        \
-        );                                                                     \
-        goto *(&&$Label);                                                      \
-      }                                                                        \
-    }                                                                          \
-    while(0);                                                                  \
+#define goto_assert($Label, $Expression, ...)                                                                          \
+  ((void)({                                                                                                            \
+    do {                                                                                                               \
+      if(!($Expression))                                                                                               \
+      {                                                                                                                \
+        clog_assert((__FILE__), (__func__), (__LINE__), (__clog_stringify($Expression)), (false), ##__VA_ARGS__);      \
+        goto *(&&$Label);                                                                                              \
+      }                                                                                                                \
+    }                                                                                                                  \
+    while(0);                                                                                                          \
   }))
 
 void
@@ -61,7 +43,6 @@ clog_assert(
   uint64_t a_line,
   const char *a_expression,
   bool a_abort,
-  uint64_t a_argc,
   ...
 );
 
@@ -75,20 +56,20 @@ typedef struct clog_dump_lookaround_s
   uint32_t m_lookahead;
 } clog_dump_lookaround_t;
 
-#define dump($Value, $ValueSize, ...)                                          \
-  ((void)({                                                                    \
-    wchar_t l_address[48];                                                     \
-    ((void)(swprintf(l_address, sizeof(l_address), L"%p", $Value)));           \
-    ((void)(clog_dump(                                                         \
-      (__FILE__),                                                              \
-      (__func__),                                                              \
-      (__LINE__),                                                              \
-      (l_address),                                                             \
-      (clog_dump_lookaround_t){0, ##__VA_ARGS__},                              \
-      (__clog_stringify($Value)),                                              \
-      ($ValueSize),                                                            \
-      ((char *)($Value))                                                       \
-    )));                                                                       \
+#define dump($Value, $ValueSize, ...)                                                                                  \
+  ((void)({                                                                                                            \
+    wchar_t l_address[48];                                                                                             \
+    ((void)(swprintf(l_address, sizeof(l_address), L"%p", $Value)));                                                   \
+    ((void)(clog_dump(                                                                                                 \
+      (__FILE__),                                                                                                      \
+      (__func__),                                                                                                      \
+      (__LINE__),                                                                                                      \
+      (l_address),                                                                                                     \
+      (clog_dump_lookaround_t){0, ##__VA_ARGS__},                                                                      \
+      (__clog_stringify($Value)),                                                                                      \
+      ($ValueSize),                                                                                                    \
+      ((char *)($Value))                                                                                               \
+    )));                                                                                                               \
   }))
 
 void
