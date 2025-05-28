d76bbc4da8a376f76a33cbcad999a2891fbae148
Dymitr <43708417+ay0ks@users.noreply.github.com>
2025-05-28 02:16:35 +0200

    fix(clog/clog.c): no need for a_argc

--- src/clog.c
+++ src/clog.c
@@ -19,16 +19,15 @@
 #  include <unistd.h>
 #endif
 
-#define __clog_minmax_helper($Op, $1, $2)                                      \
+#define __clog_minmax_helper($Op, $1, $2)                                                                              \
   ((((typeof($1))($1))$Op((typeof($2))($2))) ? ((typeof($1))($1)) : ((typeof($2))($2)))
 #define __clog_min($1, $2) __clog_minmax_helper(<, $1, $2)
 #define __clog_max($1, $2) __clog_minmax_helper(>, $1, $2)
 
-#define __clog_clamp($1, $2, $3)                                               \
-  ((((typeof($2))($2)) < ((typeof($1))($1)))                                   \
-     ? ((typeof($1))($1))                                                      \
-     : (((typeof($2))($2)) > ((typeof($3))($3)) ? ((typeof($3))($3))           \
-                                                : ((typeof($2))($2))))
+#define __clog_clamp($1, $2, $3)                                                                                       \
+  ((((typeof($2))($2)) < ((typeof($1))($1)))                                                                           \
+     ? ((typeof($1))($1))                                                                                              \
+     : (((typeof($2))($2)) > ((typeof($3))($3)) ? ((typeof($3))($3)) : ((typeof($2))($2))))
 
 bool g_locale_set = false;
 
@@ -39,32 +38,42 @@ clog_assert(
   uint64_t a_line,
   const char *a_expression,
   bool a_abort,
-  uint64_t a_argc,
   ...
 )
 {
-  if(!g_locale_set) { setlocale(LC_ALL, ""); }
+  if(!g_locale_set)
+  {
+    setlocale(LC_ALL, "");
+  }
   va_list l_args;
   va_start(l_args, a_abort);
-  char *l_message = nullptr;
-  char *l_message_2 = nullptr;
-  if(a_argc > 0) { l_message = va_arg(l_args, char *); }
-  if(a_argc > 1)
+  char *l_message = va_arg(l_args, char *);
+  char *l_message_2 = l_message;
+  if(l_message != NULL)
   {
     va_list l_args_2;
     va_copy(l_args_2, l_args);
-    uint64_t l_message_size = vsnprintf(nullptr, 0, l_message, l_args_2);
+    uint64_t l_message_size = vsnprintf(nullptr, 0, l_message, l_args_2) + 1;
     va_end(l_args_2);
     l_message_2 = malloc(l_message_size);
     assert(l_message_2 != NULL);
     vsnprintf(l_message_2, l_message_size, l_message, l_args);
   }
-  else { l_message_2 = l_message; }
   va_end(l_args);
   fprintf(stderr, "%s:%s:%lu: assertion failed\n  expression: %s", a_file, a_function, a_line, a_expression);
-  if(l_message != nullptr) { fprintf(stderr, "\n  reason: %s", l_message_2); }
+  if(l_message != NULL)
+  {
+    fprintf(stderr, "\n  reason: %s", l_message_2);
+    if(l_message_2 != l_message)
+    {
+      free(l_message_2);
+    }
+  }
   fprintf(stderr, "\n");
-  if(a_abort) { clog_abort(); }
+  if(a_abort)
+  {
+    clog_abort();
+  }
 }
 
 void
@@ -90,7 +99,10 @@ clog_dump(
   char *a_value
 )
 {
-  if(!g_locale_set) { setlocale(LC_ALL, ""); }
+  if(!g_locale_set)
+  {
+    setlocale(LC_ALL, "");
+  }
   uint64_t l_size_total = a_size + a_lookaround.m_lookbehind + a_lookaround.m_lookahead;
   char *l_value = a_value - a_lookaround.m_lookbehind;
   bool l_tty = (bool)isatty(fileno(stderr));
@@ -107,30 +119,20 @@ clog_dump(
     a_lookaround.m_lookahead,
     l_size_total
   );
-  fprintf(
-    stderr, "  offset  hex                                              ascii\n"
-  );
+  fprintf(stderr, "  offset  hex                                              ascii\n");
   for(uint64_t l_u = 0; l_u < l_size_total; l_u += 16)
   {
     fprintf(stderr, "  ");
     if(a_lookaround.m_lookbehind > 0 && l_u < a_lookaround.m_lookbehind)
     {
-      fprintf(
-        stderr,
-        "%06lx",
-        __clog_clamp(0, a_lookaround.m_lookbehind - l_u, a_lookaround.m_lookbehind)
-      );
+      fprintf(stderr, "%06lx", __clog_clamp(0, a_lookaround.m_lookbehind - l_u, a_lookaround.m_lookbehind));
     }
     else if(a_lookaround.m_lookbehind > 0
             && a_lookaround.m_lookahead > 0
             && l_u > a_size + a_lookaround.m_lookbehind
             && l_u <= a_size + a_lookaround.m_lookbehind + a_lookaround.m_lookahead)
     {
-      fprintf(
-        stderr,
-        "%06lx",
-        __clog_clamp(0, l_u - a_lookaround.m_lookbehind, a_size + a_lookaround.m_lookahead)
-      );
+      fprintf(stderr, "%06lx", __clog_clamp(0, l_u - a_lookaround.m_lookbehind, a_size + a_lookaround.m_lookahead));
     }
     else
     {
@@ -155,7 +157,10 @@ clog_dump(
           );
         }
         fprintf(stderr, "%02X ", l_byte);
-        if(l_tty) { fprintf(stderr, "\x1b[0m"); }
+        if(l_tty)
+        {
+          fprintf(stderr, "\x1b[0m");
+        }
       }
     }
     fprintf(stderr, " ");
@@ -177,7 +182,10 @@ clog_dump(
           );
         }
         fprintf(stderr, "%c", isprint(l_byte) ? l_byte : '.');
-        if(l_tty) { fprintf(stderr, "\x1b[0m"); }
+        if(l_tty)
+        {
+          fprintf(stderr, "\x1b[0m");
+        }
       }
     }
     fprintf(stderr, "\n");
